4c57f3601275855f9b975c24e54e01aa35890fdb
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-12-04 16:58:03 -0500

    Proyect basic

---
+++ app/src/main/java/unicauca/movil/preference/MainActivity.java
@@ -0,0 +1,27 @@
+package unicauca.movil.preference;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unicauca.movil.preference.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setHandler(this);
+    }
+
+    public void save(){
+
+    }
+
+    public void show(){
+        String info = binding.info.getText().toString();
+    }
+}
