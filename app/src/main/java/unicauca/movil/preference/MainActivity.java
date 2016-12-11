3723defc68213dc69b0838938a78c9b0d89a119a
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-12-10 23:41:50 -0500

    Proyect finished

--- app/src/main/java/unicauca/movil/preference/MainActivity.java
+++ app/src/main/java/unicauca/movil/preference/MainActivity.java
@@ -1,5 +1,6 @@
 package unicauca.movil.preference;
 
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,19 +10,29 @@ import unicauca.movil.preference.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setHandler(this);
+
+        preferences = getSharedPreferences("preferencias", MODE_PRIVATE);
+
     }
 
     public void save(){
+        String info = binding.info.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("info", info);
+        editor.apply();
 
     }
 
     public void show(){
-        String info = binding.info.getText().toString();
+        String info =  preferences.getString("info","");
+        binding.data.setText(info);
     }
 }
